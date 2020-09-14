143b831bf54ab6f26bf1b277aedd9e44d2d07837
Tristan <betttris13@yahoo.com.au>
2020-09-14 11:37:50 +0930

    Adds initial implementation and error checks
    Adds initial implementation of Julia wrapper and Julia side error checking.

--- src/SIGIL.jl
+++ src/SIGIL.jl
@@ -1,5 +1,65 @@
+## TODO
+# Fix directory to defult to file location
+
 module SIGIL
 
-greet() = print("Hello World!")
+using Cxx
+using Libdl
+
+WINDOW_OPEN = false
+include("SIGIL_CORE.jl")
+
+export
+# main functions
+    slWindow,
+    slShowCursor,
+    slClose,
+    slShouldClose,
+    slGetKey,
+    slGetMouseButton,
+    slGetMouseX,
+    slGetMouseY,
+    slGetDeltaTime,
+    slGetTime,
+    slRender,
+    slSetBackColor,
+    slSetForeColor,
+    slSetAdditiveBlend,
+    slPush,
+    slPop,
+    slTranslate,
+    slRotate,
+    slScale,
+    slLoadTexture,
+    slLoadWAV,
+    slSoundPlay,
+    slSoundLoop,
+    slSoundPause,
+    slSoundStop,
+    slSoundPauseAll,
+    slSoundResumeAll,
+    slSoundStopAll,
+    slSoundPlaying,
+    slSoundLooping,
+    slTriangleFill,
+    slTriangleOutline,
+    slRectangleFill,
+    slRectangleOutline,
+    slCircleFill,
+    slCircleOutline,
+    slSemiCircleFill,
+    slSemiCircleOutline,
+    slPoint,
+    slLine,
+    slSetSpriteTiling,
+    slSetSpriteScroll,
+    slSprite,
+    slSetTextAlign,
+    slGetTextWidth,
+    slGetTextHeight,
+    slLoadFont,
+    slSetFont,
+    slSetFontSize,
+    slText
 
 end # module
